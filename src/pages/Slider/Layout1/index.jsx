0bf805e7194adb0f6c45bbab4b625b52f96057a0
Alfredo Arreola <alfredo@sfleet.mx>
2020-04-01 16:06:32 -0600

    changes

--- src/pages/Slider/Layout1/index.jsx
+++ src/pages/Slider/Layout1/index.jsx
@@ -1,38 +1,37 @@
 import React from 'react'
 import {
-    TitleAlert,
-    Title,
-    Arrow,
-    Container,
-    Image,
-    Person,
+  TitleAlert,
+  Title,
+  Arrow,
+  Container,
+  Image,
+  Person,
 } from '../styles.js'
 
-function LayoutOne() {
-    return (
-        <>
-            <Image>
-                <img src={require('../../../images/hello.svg')} alt="Persona con una laptop" style={{ height: 'auto', width: '125px' }} />
-            </Image>
-            <Person>
-                <img src={require('../../../images/Person.svg')} alt="Persona con un megafono" style={{ height: 'auto', width: '145px' }} />
-            </Person>
-            <TitleAlert>
-                <Title>Comunicado Administradores</Title>
-                <Arrow />
-            </TitleAlert>
-            <Container>
-                <p style={{ textAlign: 'justify' }}>
-                    En S·Fleet nos estamos renovando para ti
-                </p>
-                <p style={{ textAlign: 'justify' }}>
-                    Ahora también podrás autorizar servicios desde tu correo electrónico, desde cualquier lugar, hora y cualquier dispositivo.
-                </p>
-                <p style={{ textAlign: 'justify' }}>Si tu cuenta de correo de acceso a la plataforma está validada recibirás un mail cuando el proveedor cargue el presupuesto de los servicios solicitados. </p>
-                <p style={{ textAlign: 'justify' }}>Te indicaremos nombre de los servicios solicitados, cantidad y monto de los mismos; también te indicaremos hace cuántos km y meses se realizó un concepto igual al solicitado y el número de folio para que des seguimiento en go.sfleet.mx.</p>
-            </Container>
-        </>
-    )
+function LayoutOne(props) {
+  console.log(props.release)
+  const listItems = props.release.paragraphs.map((item, key) =>
+    <p key={key} style={{ textAlign: 'justify' }}>
+      {item}
+    </p>
+  );
+  return (
+    <>
+      <Image>
+        <img src={require('../../../images/hello.svg')} alt="Persona con una laptop" style={{ height: 'auto', width: '125px' }} />
+      </Image>
+      <Person>
+        <img src={require('../../../images/Person.svg')} alt="Persona con un megafono" style={{ height: 'auto', width: '145px' }} />
+      </Person>
+      <TitleAlert>
+        <Title>{props.release.title}</Title>
+        <Arrow />
+      </TitleAlert>
+      <Container>
+        {listItems}
+      </Container>
+    </>
+  )
 }
 
 export default LayoutOne
--- src/pages/Slider/index.jsx
+++ src/pages/Slider/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { RightOutlined, LeftOutlined } from '@ant-design/icons'
 import LayoutOne from './Layout1'
 import LayoutTwo from './Layout2'
@@ -10,18 +10,73 @@ import {
   CircleRight
 } from './styles.js'
 function Slider() {
+  const [position, setPosition] = useState(0)
+  const [show, setShow] = useState('')
+  const arrayRelease = [
+    {
+      type: 'layout1',
+      title: 'Comunicado Administradores',
+      paragraphs: [
+        "En S·Fleet nos estamos renovando para ti",
+        "Ahora también podrás autorizar servicios desde tu correo electrónico, desde cualquier lugar, hora y cualquier dispositivo.",
+        "Si tu cuenta de correo de acceso a la plataforma está validada recibirás un mail cuando el proveedor cargue el presupuesto de los servicios solicitados.",
+        "Te indicaremos nombre de los servicios solicitados, cantidad y monto de los mismos; también te indicaremos hace cuántos km y meses se realizó un concepto igual al solicitado y el número de folio para que des seguimiento en go.sfleet.mx."
+      ]
+    },
+    {
+      type: 'layout2',
+      image: '../../../images/example.png',
+    },
+    {
+      type: 'layout3',
+      note: 'Si desconoces el proceso para validar tu cuenta de correo, comunícate con nuestra área de servicio a cliente.',
+      contacts: [
+        {
+          name: 'Gloria Barrios',
+          workstation: 'Gerente Atención a Clientes',
+          phone: '(55) 6792 2318',
+          email: 'gloria@sfleet.mx',
+        },
+        {
+          name: 'Camila Arreola',
+          workstation: 'Ejecutivo Atención a Clientes',
+          phone: '(55) 8842 7643 ext. 203',
+          email: 'jose.jimenez@sfleet.mx',
+        }
+      ]
+    }
+  ]
+  useEffect(() => {
+    setShow(arrayRelease[position].type === 'layout1' ? <LayoutOne release={arrayRelease[position]} /> : arrayRelease[0].type === 'layout2' ? <LayoutTwo release={arrayRelease[position]} /> : <LayoutThree release={arrayRelease[position]} />)
+    console.log(arrayRelease)
+  }, [])
+
+  function showLayout(direction) {
+    debugger
+    let counter = position
+    if (direction === 'left') {
+      if(position !== 0){
+        counter = counter -1
+        setPosition(counter)
+      }
+    } else {
+      if(position < (arrayRelease.length-1)){
+        counter = counter  + 1
+        setPosition(counter)
+      }
+    }
+    setShow(arrayRelease[counter].type === 'layout1' ? <LayoutOne release={arrayRelease[counter]} /> : arrayRelease[counter].type === 'layout2' ? <LayoutTwo release={arrayRelease[counter]} /> : <LayoutThree release={arrayRelease[counter]} />)
+  }
   return (
     <SliderContainer>
       <CircleClose />
-      <CircleLeft>
+      <CircleLeft onClick={() => showLayout('left')}>
         <LeftOutlined style={{ fontSize: '20px', color: '#fff' }} />
       </CircleLeft>
-      <CircleRight>
+      <CircleRight onClick={() => showLayout('')}>
         <RightOutlined style={{ fontSize: '20px', color: '#fff' }} />
       </CircleRight>
-      {/* <LayoutOne /> */}
-      {/* <LayoutTwo /> */}
-      <LayoutThree />
+      {show}
     </SliderContainer>
   )
 }
